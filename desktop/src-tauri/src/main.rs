5890c242153a988d25d32fd92d0b9f64a2591e6a
Horst Herb <my.list.subscriptions@gmail.com>
2026-07-03 10:56:47 +0930

    feat(desktop): backend pick_* dialog commands populate approval slots (#353)

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -21,7 +21,7 @@ use secretary_desktop::commands::lock::{
     vault_locked_payload, LOCK_REASON_AUTO, VAULT_LOCKED_EVENT,
 };
 use secretary_desktop::commands::{
-    browse, contacts, create, delete, edit, lock, reauth, settings, sync, unlock, vault,
+    browse, contacts, create, delete, edit, lock, pick, reauth, settings, sync, unlock, vault,
 };
 use secretary_desktop::constants::AUTO_LOCK_TICK_MS;
 use secretary_desktop::session::VaultSession;
@@ -84,6 +84,9 @@ fn main() {
             browse::reveal_field,
             create::create_vault,
             create::probe_create_target,
+            pick::pick_vault_folder,
+            pick::pick_contact_card,
+            pick::pick_export_dir,
             edit::create_block,
             edit::rename_block,
             edit::save_record,
